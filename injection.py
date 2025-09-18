fb7106fe07e3fde0f0e04c669feb14fc00986101
Alfie Lamerton <alfie.david.lamerton@gmail.com>
2025-09-18 15:54:34 +0100

    Initial commit pre-runpod

---
+++ injection.py
@@ -0,0 +1,17 @@
+# %% Imports
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+# %%
+model_id = "meta-llama/Llama-2-7b-hf"
+
+# Load model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, torch_dtype=torch.float16, device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+# Example inference
+inputs = tokenizer("Hello, how are you?", return_tensors="pt").to(model.device)
+outputs = model.generate(**inputs)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
